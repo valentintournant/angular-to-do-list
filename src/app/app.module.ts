b672211a62c46621983daab071d434272ca66d5c
Valentin T <valentin.tournant@gmail.com>
2021-06-25 15:46:17 +0200

    intégration feuilles de styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { TodoComponent } from "./todo/todo.component";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
 declarations: [
   AppComponent,
-  TodoComponent
+  TodoComponent,
+  HeaderComponent
 ],
 imports: [
   BrowserModule,
