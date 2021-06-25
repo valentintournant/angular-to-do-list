75d738cc703e8ef1764283f3ed12c00103b826d4
Valentin T <valentin.tournant@gmail.com>
2021-06-25 14:46:42 +0200

    logique component first todo

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'my-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+
+})
+
+export class TodoComponent{
+
+}
