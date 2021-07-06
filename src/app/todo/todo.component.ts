bebc2a3a76cddf3ba12059826095d17ecb53e0a8
Valentin T <valentin.tournant@gmail.com>
2021-07-06 10:13:49 +0200

    propety biding and event binding for status

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,10 +8,46 @@ import { Component } from "@angular/core";
 })
 
 export class TodoComponent{
-  todoOne: string = "Projet 1";
-  todoTwo: string = "Projet 2";
-  todoThree: string = "Projet 3";
-  todoFour: string = "Projet 4";
 
-  todos: string[] = ["Projet t1", "Projet t2", "Projet t3", "Projet t4", "Projets T6", "Projet t8"];
+  // todoOne: string = "Projet 1";
+  // todoTwo: string = "Projet 2";
+  // todoThree: string = "Projet 3";
+  // todoFour: string = "Projet 4";
+
+  // todos: string[] = ["Projet t1", "Projet t2", "Projet t3", "Projet t4"];
+
+  todos = [
+    {
+      todoName: "Projet 1",
+      todoStatus: true,
+      image: "http://placehold.it/150"
+    },
+    {
+      todoName: "Projet 2",
+      todoStatus: false,
+      image: "http://placehold.it/150"
+    },
+    {
+      todoName: "Projet 3",
+      todoStatus: true,
+      image: "http://placehold.it/150"
+    },
+    {
+      todoName: "Projet 4",
+      todoStatus: false,
+      image: "http://placehold.it/150"
+    },
+    {
+      todoName: "Projet 5",
+      todoStatus: true,
+      image: "http://placehold.it/150"
+    },
+  ];
+
+  onChangeStatus(i: number){
+    this.todos[i].todoStatus = !this.todos[i].todoStatus;
+  }
+
+
+
 }
