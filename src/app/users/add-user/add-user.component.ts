513a847dcfd66a0d626a98ba87c6f683fe3c19c8
Valentin T <valentin.tournant@gmail.com>
2021-07-27 15:23:43 +0200

    end reactive method with add user : create aliases

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -1,9 +1,10 @@
 import { UsersService } from './../../services/users.service';
 import { Address } from 'src/app/models/address.model';
 import { User } from 'src/app/models/user.models';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-add-user',
@@ -34,22 +35,36 @@ export class AddUserComponent implements OnInit {
         state: this.formBuilder.control("", Validators.required),
         zip: this.formBuilder.control("", Validators.required),
         city: this.formBuilder.control("", Validators.required),
-      })
+      }),
+      aliases: this.formBuilder.array([]),
     });
   }
 
+  getAliases(): FormArray {
+    return this.userForm.get("aliases") as FormArray;
+  }
+
+  addAliases(): void {
+    this.getAliases().push(this.formBuilder.control("", Validators.required))
+  }
+
   onSubmit():void {
+
     const dataUser = this.userForm.value;
+    console.log(dataUser);
     const address = new Address(dataUser.street, dataUser.state, dataUser.zip, dataUser.city);
-    const newUser = new User(dataUser.firstname,
+    const alias = dataUser.aliases ? dataUser.aliases : [];
+    const newUser = new User(
+      dataUser.firstname,
       dataUser.lastname,
       dataUser.email,
       address,
       dataUser.description,
-      dataUser.dataBirth);
+      dataUser.dataBirth ,
+      alias
+      );
       this.userService.addUser(newUser);
-      this.router.navigate(["users"])
-    // console.log(dataUser.firstname);
+      this.router.navigate(["users"]);
   }
 }
 
